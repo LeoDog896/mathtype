386e8e893c3ee81ccc6917ed529b796baf2f3cba
LeoDog896 <LeoDog896@hotmail.com>
2022-06-16 12:15:05 +0000

    Bump mathquill, increase accessibility & navigation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,62 @@
 <script lang="ts">
   import { MathQuill } from "svelte-mathquill";
   import { autofocus } from "$lib/autofocus";
+  import { tick } from "svelte"
 
   type Piece = {
-    type: "text" | "math";
     content: string;
-    editable?: boolean;
-  }
+  } & ({ type: "text" } | { type: "math", element?: MathQuill })
 
   interface Section {
     name: string;
     pieces: Piece[];
-    editable?: boolean;
   }
 
   let sections: Section[] = []
 
-  function addSection() {
+  async function addSection() {
     sections = [...sections, {
       name: "New Section",
-      pieces: []
+      pieces: [{ type: "math", content: "" }]
     }]
-  }
-
-  function addPiece(section: Section, piece: Piece) {
-    section.pieces = [...section.pieces, piece]
   }
 </script>
 
 <div class="m-8">
   {#each sections as section}
     <div class="block">
-      {#if section.editable}
-        <input bind:value={section.name} use:autofocus on:blur={() => section.editable = false} placeholder="Section Name" class="text-bold text-lg">
-      {:else}
-        <p class="text-bold text-lg" on:dblclick={() => section.editable = true}>{section.name}</p>
-      {/if}
+      <input use:autofocus bind:value={section.name} use:autofocus placeholder="Section Name" class="text-bold text-lg">
       <div class="border-l border-gray-300 p-4">
-        {#each section.pieces as piece}
+        {#each section.pieces as piece, i}
           {#if piece.type == "text"}
-            {#if piece.editable}
-              <input on:blur={() => piece.editable = false} bind:value={piece.content} placeholder="Enter text..."/>
-            {:else}
-              <p on:dblclick={() => piece.editable = true}>{piece.content}</p>
-            {/if}
+            <input bind:value={piece.content} placeholder="Enter text..."/>
           {:else if piece.type == "math"}
             <div class="mb-4">
-              <MathQuill bind:latex={piece.content}/><br/>
+              <MathQuill bind:this={piece.element} bind:latex={piece.content}
+              
+              on:enter={async () => {
+                section.pieces = [...section.pieces, { type: "math", content: "" }]
+                await tick()
+                const nextPiece = section.pieces[i + 1]
+                if (nextPiece.type == "math")
+                  nextPiece.element?.focus()
+              }}
+
+              on:delete={async () => {
+                if (section.pieces.length === 1) return
+                section.pieces = section.pieces.filter((_, index) => index !== i)
+                await tick()
+                const nextPiece = section.pieces[i - 1]
+                if (nextPiece.type == "math")
+                  nextPiece.element?.focus()
+              }}
+              /><br/>
             </div>
           {/if}
         {/each}
       </div>
-      <button class="print:hidden" on:click={() => { section.pieces = [...section.pieces, { type: "math", content: "\\sqrt{x}" }] }}>Add Piece</button>
     </div>
   {/each}
 
-  <button class="print:hidden mt-4 px-4 py-2 rounded-lg bg-blue-300 ring active:ring-blue-200" on:click={addSection}>Add Section</button>
+  <button class="print:hidden mt-4 px-4 py-2 rounded-full border border-black" on:click={addSection}>Add Section</button>
 </div>
